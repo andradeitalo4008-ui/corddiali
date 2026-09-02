3681677d8a3efe2adb36f7da9517742f54e21b47
andradeitalo4008-ui <andradeitalo4008@gmail.com>
2026-09-02 11:10:32 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -139,7 +139,7 @@ export default function Home() {
     </Reveal>
 
    <Reveal delay={0.1}>
-  <div className="flex justify-center md:justify-start md:pt-60 md:-ml24">
+  <div className="flex justify-center md:justify-start md:pt-60 md:-ml48">
     <img
       src="/images/imagen-ceos.JPEG"
       className="w-full max-w-[400px] h-auto object-cover"
