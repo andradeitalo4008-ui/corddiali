756e8ed94df7d37aaa2f05c21fd215d9d7e82d23
andradeitalo4008-ui <andradeitalo4008@gmail.com>
2026-09-08 19:03:24 -0300

    Update page.tsx

--- app/corddiali/page.tsx
+++ app/corddiali/page.tsx
@@ -72,7 +72,7 @@ export default function Empresa() {
           </div>
 
           {/* Guilherme */}
-          <div className="grid md:grid-cols-2 gap-10 lg:gap-16 items-center mt-20">
+          <div className="grid md:grid-cols-2 gap-10 lg:gap-16 items-center mt-8">
 
             <div className="flex justify-center md:justify-start">
               <img
