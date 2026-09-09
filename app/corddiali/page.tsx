439ed31147691ec0ec3492bb84fa8f3406ca1882
andradeitalo4008-ui <andradeitalo4008@gmail.com>
2026-09-09 12:27:08 -0300

    Update page.tsx

--- app/corddiali/page.tsx
+++ app/corddiali/page.tsx
@@ -84,7 +84,7 @@ export default function Empresa() {
             </div>
 
             {/* Texto Guilherme */}
-            <div className="md:pl-6">
+          <div className="md:pl-6 md:-translate-y-20">
               <div className="eyebrow">Eng. Guilherme De Carli</div>
 
               <h2 className="display text-3xl md:text-5xl mt-4">
